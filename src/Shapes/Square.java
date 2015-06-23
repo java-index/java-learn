c2e0b138b7a181e4710730452fe090719c79f79e
Roman <prv@ukr.net>
2015-06-23 18:13:49 +0300

    work 23062105

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -6,7 +6,7 @@ public class Square extends Rectangle {
 
     @Override
     protected void draw(Graphics g) {
-        System.out.println("Rectangle DRAW");
+        System.out.println("Square DRAW");
         g.setColor(new Color(34, 0, 255));
         g.drawRect(349, 149, 51, 51);
         g.setColor(new Color(255, 0, 52));
