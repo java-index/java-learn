b77a5c12a3331b1a4694418381f00621e790fdcf
coderator <boke@ukr.net>
2015-07-02 08:14:20 +0300

    Merge branch 'master' of https://github.com/java-index/HomeWork
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/LinkedList/SimpeLinkedList.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+import java.util.Objects;
+
+/**
+ * Created by rabota on 01.07.15.
+ */
+public class SimpeLinkedList {
+    Node root;
+    int size;
+
+    public void addFirst(Object o){
+
+    }
+
+    public void addLast(Object o){
+
+    }
+
+    public void addAfter(Object o, Objects j){
+
+    }
+
+    public int getSize(){
+        return size;;
+    }
+
+
+
+
+
+}
